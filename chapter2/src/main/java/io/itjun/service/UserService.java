a73ef9568cee27f1d84bc5ac31f49a23b425ce1c
黄荣君 <l1091462907@gmail.com>
2019-01-01 23:49:00 +0800

    format code

--- chapter2/src/main/java/io/itjun/service/UserService.java
+++ chapter2/src/main/java/io/itjun/service/UserService.java
@@ -1,48 +1,46 @@
 package io.itjun.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import io.itjun.dao.LoginLogDao;
 import io.itjun.dao.UserDao;
 import io.itjun.domain.LoginLog;
 import io.itjun.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
-	private UserDao userDao;
-	private LoginLogDao loginLogDao;
+    private UserDao userDao;
+    private LoginLogDao loginLogDao;
 
 
-	public boolean hasMatchUser(String userName, String password) {
-		int matchCount =userDao.getMatchCount(userName, password);
-		return matchCount > 0;
-	}
-	
-	public User findUserByUserName(String userName) {
-		return userDao.findUserByUserName(userName);
-	}
+    public boolean hasMatchUser(String userName, String password) {
+        int matchCount = userDao.getMatchCount(userName, password);
+        return matchCount > 0;
+    }
 
-	@Transactional
+    public User findUserByUserName(String userName) {
+        return userDao.findUserByUserName(userName);
+    }
+
+    @Transactional
     public void loginSuccess(User user) {
-		user.setCredits( 5 + user.getCredits());
-		LoginLog loginLog = new LoginLog();
-		loginLog.setUserId(user.getUserId());
-		loginLog.setIp(user.getLastIp());
-		loginLog.setLoginDate(user.getLastVisit());
+        user.setCredits(5 + user.getCredits());
+        LoginLog loginLog = new LoginLog();
+        loginLog.setUserId(user.getUserId());
+        loginLog.setIp(user.getLastIp());
+        loginLog.setLoginDate(user.getLastVisit());
         userDao.updateLoginInfo(user);
         loginLogDao.insertLoginLog(loginLog);
-	}
+    }
 
-	@Autowired
-	public void setUserDao(UserDao userDao) {
-		this.userDao = userDao;
-	}
+    @Autowired
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
-	@Autowired
-	public void setLoginLogDao(LoginLogDao loginLogDao) {
-		this.loginLogDao = loginLogDao;
-	}
+    @Autowired
+    public void setLoginLogDao(LoginLogDao loginLogDao) {
+        this.loginLogDao = loginLogDao;
+    }
 }
